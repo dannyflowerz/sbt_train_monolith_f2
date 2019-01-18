1f2d4397396bc6e0a1136d3de44649d186c78ab8
dannyflowerz <daniel.gabor.flores@gmail.com>
2019-01-18 14:58:43 +0100

    postman collection added

--- src/main/java/io/blueharvest/training/ms/monolith/Constants.java
+++ src/main/java/io/blueharvest/training/ms/monolith/Constants.java
@@ -2,6 +2,6 @@ package io.blueharvest.training.ms.monolith;
 
 public final class Constants {
 
-    public final static String NO_PRODUCT_AVAILABLE_CODE = "N/A";
+    public static final String NO_PRODUCT_AVAILABLE_CODE = "N/A";
 
 }
